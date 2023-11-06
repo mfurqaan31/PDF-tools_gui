25d129d598e68c481e2aa616d264a087ea467fdd
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2023-11-06 12:56:46 +0530

    Updated addpg_pdf.py

--- addpg_pdf.py
+++ addpg_pdf.py
@@ -1,12 +1,10 @@
-"""
 import fitz  # PyMuPDF
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ExifTags
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
-import io
+from io import BytesIO
 
 class PDFEditorApp:
     def __init__(self, root):
@@ -16,6 +14,7 @@ class PDFEditorApp:
 
         self.pdf_document = None
         self.current_page = 0
+        self.temp_images = []  # List to hold temporary images to display
 
         self.create_gui()
 
@@ -24,6 +23,7 @@ class PDFEditorApp:
         self.prev_button = tk.Button(self.root, text="Previous Page", command=self.prev_page, bg="black", fg="white")
         self.next_button = tk.Button(self.root, text="Next Page", command=self.next_page, bg="black", fg="white")
         self.add_page_button = tk.Button(self.root, text="Add Page", command=self.add_page, bg="black", fg="white")
+        self.add_to_last_page_button = tk.Button(self.root, text="Add to Last Page", command=self.add_to_last_page, bg="black", fg="white")
         self.make_pdf_button = tk.Button(self.root, text="Make PDF", command=self.save_pdf, bg="black", fg="white")
 
         # Create a canvas to display the PDF page
@@ -39,6 +39,7 @@ class PDFEditorApp:
         self.prev_button.pack()
         self.next_button.pack()
         self.add_page_button.pack()
+        self.add_to_last_page_button.pack()
         self.make_pdf_button.pack()
 
     def load_pdf(self):
@@ -68,27 +69,48 @@ class PDFEditorApp:
             self.show_page()
 
     def next_page(self):
-        if self.current_page < len(self.pdf_document) - 1:
+        if self.pdf_document is not None and self.current_page < len(self.pdf_document) - 1:
             self.current_page += 1
             self.show_page()
 
     def add_page(self):
         if self.pdf_document is not None:
-            file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg;*.jpeg;*.png;*.bmp;*.gif")])
+            file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tiff")])
             if file_path:
-                new_pdf = fitz.open()
-                new_page = new_pdf.new_page(width=400, height=600)
-                new_page.insert_image(fitz.Rect(0, 0, 400, 600), filename=file_path)
+                self.insert_page(file_path)
+                self.show_page()  # Display the inserted page in the GUI
 
-                # Merge the new PDF with the original PDF
-                merged_pdf = fitz.open()
-                merged_pdf.insert_pdf(self.pdf_document)
-                merged_pdf.insert_pdf(new_pdf)
+    def add_to_last_page(self):
+        if self.pdf_document is not None:
+            file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.jpg *.jpeg *.png *.bmp *.gif *.tiff")])
+            if file_path:
+                self.insert_page(file_path, page_index=len(self.pdf_document))  # Insert at the end
+                self.show_page()  # Display the inserted page in the GUI
 
-                # Update the current PDF document
-                self.pdf_document = merged_pdf
-
-                self.show_page()
+    def insert_page(self, image_path, page_index=None):
+        if image_path:
+            if page_index is None:
+                page_index = self.current_page
+            new_page = self.pdf_document.new_page(page_index, width=500, height=700)
+            rect = fitz.Rect(0, 0, 500, 700)
+            
+            # Extract rotation information from the image (EXIF) and apply it to the new page
+            img = Image.open(image_path)
+            exif_data = img._getexif()
+            if exif_data:
+                for tag, value in exif_data.items():
+                    if tag in ExifTags.TAGS and ExifTags.TAGS[tag] == "Orientation":
+                        if value == 3:
+                            new_page.set_rotation(180)
+                        elif value == 6:
+                            new_page.set_rotation(90)
+                        elif value == 8:
+                            new_page.set_rotation(270)
+            
+            image_pixmap = fitz.Pixmap(image_path)
+            new_page.insert_image(rect, pixmap=image_pixmap)
+            temp_image = Image.open(image_path)
+            self.temp_images.insert(page_index, temp_image)
 
     def save_pdf(self):
         if self.pdf_document is not None:
@@ -103,16 +125,12 @@ class PDFEditorApp:
             if save_path:
                 self.pdf_document.save(save_path)
                 self.pdf_document.close()
-                self.pdf_document = fitz.open(save_path)
-                self.current_page = 0
-                self.show_page()
                 messagebox.showinfo("PDF Saved", "PDF has been saved successfully.")
                 self.root.quit()  # Exit the program
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = PDFEditorApp(root)
-    root.withdraw()  # Hide the GUI window initially
+    app.root.withdraw()  # Hide the GUI window initially
     app.load_pdf()
     root.mainloop()
-"""
