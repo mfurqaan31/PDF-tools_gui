88cf3ad56e24e5d04bb5cafc1fbbf66d9b4fe729
Mohammed Furqaan <furqaanpatel2004@gmail.com>
2023-11-06 15:13:57 +0530

    Updated pdf_merge.py

--- pdf_merge.py
+++ pdf_merge.py
@@ -9,10 +9,10 @@ root = tk.Tk()
 root.withdraw()
 
 # Prompt the user to select PDF files from a folder
-pdf_files = filedialog.askopenfilenames(
+pdf_files = list(filedialog.askopenfilenames(
     title="Select PDF Files",
     filetypes=[("PDF files", "*.pdf")]
-)
+))
 
 if not pdf_files:
     print("No PDF files selected. Exiting.")
@@ -33,13 +33,70 @@ else:
 # Construct the output file path with the name of the first PDF file
 output_pdf = os.path.join(downloads_dir, f'combined_{os.path.splitext(os.path.basename(first_pdf_name))[0]}.pdf')
 
-pdf_document = fitz.open()
+# Create a tkinter root window for the GUI
+root = tk.Tk()
+root.title("PDF Orderer")
+root.configure(bg='black')  # Set background color to black
 
-# Iterate through the list of selected PDF files and insert them into the document
+# Function to move the selected item in the listbox up
+def move_up():
+    selected_index = pdf_listbox.curselection()
+    if selected_index:
+        selected_index = selected_index[0]
+        if selected_index > 0:
+            item = pdf_listbox.get(selected_index)
+            pdf_listbox.delete(selected_index)
+            pdf_listbox.insert(selected_index - 1, item)
+            pdf_listbox.select_set(selected_index - 1)
+            reorder_pdf_files(selected_index, selected_index - 1)
+
+# Function to move the selected item in the listbox down
+def move_down():
+    selected_index = pdf_listbox.curselection()
+    if selected_index:
+        selected_index = selected_index[0]
+        if selected_index < pdf_listbox.size() - 1:
+            item = pdf_listbox.get(selected_index)
+            pdf_listbox.delete(selected_index)
+            pdf_listbox.insert(selected_index + 1, item)
+            pdf_listbox.select_set(selected_index + 1)
+            reorder_pdf_files(selected_index, selected_index + 1)
+
+pdf_listbox = tk.Listbox(root, selectmode=tk.SINGLE, bg='black', fg='white', width=100, height=20)
+pdf_listbox.pack(padx=20, pady=10)
+pdf_listbox.config(highlightbackground='black')  # Set the listbox border color
+
+# Function to maintain the order of the PDF files based on listbox changes
+def reorder_pdf_files(old_index, new_index):
+    pdf_files_ordered.insert(new_index, pdf_files_ordered.pop(old_index))
+
+# Function to merge the selected PDFs in the order specified
+def merge_pdfs():
+    if not pdf_files_ordered:
+        print("No PDFs selected. Nothing to merge.")
+        return
+
+    pdf_document = fitz.open()
+    for pdf_file in pdf_files_ordered:
+        pdf_document.insert_pdf(fitz.open(pdf_file))
+    pdf_document.save(output_pdf)
+    pdf_document.close()
+
+    print(f'PDFs merged and saved as "{output_pdf}"')
+    root.destroy()
+
+# Create buttons with larger gaps and text padding
+move_up_button = tk.Button(root, text="Move Up", command=move_up, padx=20, pady=10)
+move_down_button = tk.Button(root, text="Move Down", command=move_down, padx=20, pady=10)
+merge_button = tk.Button(root, text="Merge PDFs", command=merge_pdfs, padx=20, pady=10)
+
+move_up_button.pack(pady=10)
+move_down_button.pack(pady=10)
+merge_button.pack(pady=10)
+
+pdf_files_ordered = pdf_files.copy()  # To maintain the order of PDFs
+
 for pdf_file in pdf_files:
-    pdf_document.insert_pdf(fitz.open(pdf_file))
+    pdf_listbox.insert(tk.END, os.path.basename(pdf_file))
 
-pdf_document.save(output_pdf)
-pdf_document.close()
-
-print(f'Combined PDF saved as "{output_pdf}"')
+root.mainloop()
